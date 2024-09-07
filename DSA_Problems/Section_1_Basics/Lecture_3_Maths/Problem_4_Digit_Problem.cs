f2772fb38a5679afc9a0330c7e0befd258951df9
ag20373 <ag20373@gmail.com>
2024-09-07 21:41:03 +0530

    Digits problme

---
+++ DSA_Problems/Section_1_Basics/Lecture_3_Maths/Problem_4_Digit_Problem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Problems.Section_1_Basics.Lecture_3_Maths
+{
+    class Problem_4_Digit_Problem
+    {
+        public Problem_4_Digit_Problem()
+        {
+            DigitPRoblem();
+        }
+
+        public void DigitPRoblem()
+        {
+            int no = 12345;
+
+            int CountOfDigits = 0;
+            int LastDigit = no%10 ;//Get Us Last Digit
+            int SumOfDigit = 0;
+
+            while (no != 0)
+            {
+                CountOfDigits++;//Gets Count Till iteration Ends            
+                SumOfDigit = SumOfDigit + (no % 10); //Reducting number by base 10
+                no = no / 10;//Remove Last digit.
+            }
+
+            Console.WriteLine("Sum Of Digits : " + SumOfDigit);//15
+            Console.WriteLine("Last Digit : " + LastDigit);//5
+            Console.WriteLine("Count Of digits : " + CountOfDigits);//5
+
+        }
+    }
+}
+
+// Space Complexity : O(1)
+// Time Complexity :O(logbase10n)
