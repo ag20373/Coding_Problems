92a9b3778710bf35a661010378874ec12d43981b
ag20373 <ag20373@gmail.com>
2024-09-08 00:25:09 +0530

    gcd

---
+++ DSA_Problems/Section_1_Basics/Lecture_3_Maths/Problem_7_Greatest_Common_divisor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Problems.Section_1_Basics.Lecture_3_Maths
+{
+    class Problem_7_Greatest_Common_divisor
+    {
+    }
+}
